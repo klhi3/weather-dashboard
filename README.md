daf4fdbd0f867ac48aa3489e3a44ad054b98e0de
Hekwang Lhi <hekwang.lhi@gmail.com>
2021-04-28 04:03:46 -0400

    all done api

--- README.md
+++ README.md
@@ -1,38 +1,40 @@
 # Weather Dashboard
 
  to build a weather dashboard that will run in the browser with local storage and feature dynamically updated HTML and CSS using API.
-
-
+  
+  
 ## Features
 
 To check weather for a trip plan in multiple cities
 
-
-1. show a weather dashboard
+1. display a weather dashboard
 2. search by city name
-3. have a search history of cities
+3. search history of cities
 4. display information of the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-5. display weather condition in color such as favorable, moderate or severe by UV index
+5. display weather condition in color such as favorable, moderate or severe by UV index (0~2 green, 2~5 yellow, 5~7 orange, 7~10 red, 10~ violet)
 6. display a 5-day forecast with the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-7. redisplay the weather information of the city in the search history
-
-
+7. redisplay the weather information of the city in the search history  
+  
+※ temperature represents minimum temperature
+  
+    
 ## Run
 
 To view this project, click [Weather Dashboard](https://klhi3.github.io/weather-dashboard/)
 
-
+  
+  
 ## Portfolio
 
 ![](./assets/images/page.JPG)
-
-
+  
+  
 ## Resources 
 
 - [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities
 
 - [gradient](https://cssgradient.io/) to create gradient color 
-
-
+  
+  
 - - -
-:copyright:klhi3
+ⓒklhi3
--- assets/js/script.js
+++ assets/js/script.js
@@ -28,47 +28,82 @@ var handleFormSubmit = function (event) {
   dateInputEl.val('');
 };
 
-formEl.on('submit', handleFormSubmit);
+// formEl.on('submit', handleFormSubmit);
 
-// Autocomplete widget
-$(function () {
-  var skillNames = [
-    'Bootstrap',
-    'C',
-    'C++',
-    'CSS',
-    'Express.js',
-    'Git',
-    'HTML',
-    'Java',
-    'JavaScript',
-    'jQuery',
-    'JSON',
-    'MySQL',
-    'Node.js',
-    'NoSQL',
-    'PHP',
-    'Python',
-    'React',
-    'Ruby',
-  ];
-  $('#skill-name').autocomplete({
-    source: skillNames,
-  });
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var cityWeather;
+var usersContainer = document.getElementById('currentCity');
+var fetchButton = document.getElementById('btn-searchCity');
+
+
+
+function color_string(number) {
+  if (number<=2) return "green";
+  else if (number<=5) return "yellow";
+  else if (number<=7) return "orange";
+  else if (number<=10) return "red";
+  return "violet";
+}
+
+function date_string(number){
+  return moment.unix(number).format("M/D/YYYY");
+}
+
+var cityName = "New York";
+var apiKey = "c812965f3b858aaacfcb1d520a8d9fa5";
+
+var api ="http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=imperial&appid="+apiKey;
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+fetch(api)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+
+    var lon = data.coord.lon;
+    var lat = data.coord.lat;
+
+    var api_index =
+    "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly&units=imperial&appid="+apiKey;
+    fetch(api_index)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+
+      var weatherList=[];
+      var aTmp= date_string(data.current.dt);
+      cityWeather={
+         date: aTmp,
+         temp: data.current.temp,
+         wind_speed: data.current.wind_speed,
+         icon : data.current.weather[0].icon,
+         humidity: data.current.humidity,
+         uvi: data.current.uvi,
+      };
+
+      weatherList.push(cityWeather);
+
+      for(var i=1; i<6; i++){
+        aTmp = date_string(data.daily[i].dt);
+        cityWeather ={
+                date: aTmp,
+                temp: data.daily[i].temp.min,
+                wind_speed: data.daily[i].wind_speed,
+                icon : data.daily[i].weather[0].icon,
+                humidity: data.daily[i].humidity,
+                uvi: data.daily[i].uvi
+         };
+         weatherList.push(cityWeather);
+      }
+
+
+
+
+    });
+
+
 });
 
-// Datepicker widget
-$(function () {
-  $('#datepicker').datepicker({
-    changeMonth: true,
-    changeYear: true,
-  });
-});
-
-// Sortable interaction
-$(function () {
-  $('#skills-list').sortable({
-    placeholder: 'ui-state-highlight',
-  });
-  $('#skills-list').disableSelection();
-});
